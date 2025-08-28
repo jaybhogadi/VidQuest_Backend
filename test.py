bc3e96cd2cf3c18befece86ef6e4015f43e47f1c
JAYA <jayakrishnasri13@gmail.com>
2025-08-28 22:38:35 +0530

    Ignored upload folder

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import websockets
 import asyncio
 
 async def test_websocket():
-    uri = "ws://localhost:8000/ws?requestId=12345&numMCQs=5"
+    uri = "wss://vidquest-backend.onrender.com/ws?requestId=12345&numMCQs=5"
     async with websockets.connect(uri) as websocket:
         await websocket.send("Hello, WebSocket!")
         response = await websocket.recv()
